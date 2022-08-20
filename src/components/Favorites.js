ca04bebcbcb2652879e2b7d93305345567fa6658
Yiğit Doğan <yigtdogn@outlook.com>
2022-08-20 19:34:42 +0300

    Cards & Mobile App added

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -11,7 +11,7 @@ export default function Favorites(){
     },[])
 
     return(
-        <div className='container mx-auto'>
+        <div>
             <h3 className="text-sm font-semibold mb-3">Favoriler</h3>
             <div className='grid grid-cols-8 gap-0.1 rounded-lg overflow-hidden'> 
                 {products.length && products.map((product, index) => <ProductItem key={index} product={product} />)}
